6d216c8a173600e3995ddb88041702835500222e
mzhub <mishabhadhi@gmail.com>
2025-02-11 15:30:36 +0530

    k

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,13 @@ k = st.number_input(
 if k == None:
     k = np.random.randint(10000)
 
-if st.button("**Generate**"):
+# Center align buttons
+col1, col2, col3 = st.columns([1, 2, 1])
+with col2:
+    generate_button = st.button("**Generate**", use_container_width=True)
+
+
+if generate_button:
     k_str = str(k).zfill(4)
     if k_str[0] == k_str[1] == k_str[2] == k_str[3]:
         st.write("Break")
