ffc180ffb62db9d745a422fcb464e1128aab2db0
mzhub <mishabhadhi@gmail.com>
2025-02-11 20:22:37 +0530

    col4

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ if k == None:
     k = np.random.randint(10000)
 
 # Center align buttons
-col1, col2, col3 = st.columns([1, 2, 1])
+col1, col2, col3, col4 = st.columns([1, 1, 1, 1])
 with col1:
     generate_button = st.button("**Generate**", use_container_width=True)
 
@@ -25,7 +25,7 @@ if generate_button:
     time.sleep(2)
     if k_str[0] == k_str[1] == k_str[2] == k_str[3]:
         st.warning(f" The number {k} have same four digits.", icon="⚠️")
-        col1, col2, col3 = st.columns([1, 2, 1])
+        col1, col2, col3, col4 = st.columns([1, 1, 1, 1])
         with col2:
             retry_button = st.button("**Retry**", use_container_width=True)
         st.stop()
