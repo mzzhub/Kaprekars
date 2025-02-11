dc658fa7385d52f6173a2c4a8f315d0d47dadb71
mzhub <mishabhadhi@gmail.com>
2025-02-11 15:19:01 +0530

    k

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
 st.title("Kaprekars Constant Attainer 🧮")
+
+k = st.number_input("**Enetr a number**", 1, 9999, None, help = "Four digit number. Except same four digit.", placeholder = "Click Generate to calculate with a random number")
