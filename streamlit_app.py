058bf7978d52027cb9a7454d0c446b431dcfa4a0
mzhub <mishabhadhi@gmail.com>
2025-02-12 11:20:15 +0530

    html

--- streamlit_app.py
+++ streamlit_app.py
@@ -89,9 +89,11 @@ if generate_button:
             time.sleep(1)
             if last_time:
                 time.sleep(1)
-                st.info("The last two subtracted values are **6174**.\n\nSo the subtracted value will be the same for infinite times.")
+                # st.info("The last two subtracted values are **6174**.\n\nSo the subtracted value will be the same for infinite times.")
+                st.markdown("<h1 style='font-size:50px;'>The last two subtracted values are **6174**.\n\nSo the subtracted value will be the same for infinite times.</h1>", unsafe_allow_html=True)
                 time.sleep(5)
-                st.title("Hence Kaprekars Constant 6174 attained")
+                # st.title("Hence Kaprekars Constant 6174 attained")
+                st.markdown("<h1 style='font-size:50px;'>Hence Kaprekars Constant 6174 attained</h1>", unsafe_allow_html=True)
                 break
         
             
