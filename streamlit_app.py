1497d5d482b7f081f0f7d3195168d7c6e5b98edb
mzhub <mishabhadhi@gmail.com>
2025-02-12 20:46:08 +0530

    col5

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,10 +12,10 @@ k = st.number_input(
 )
 
 # Center align buttons
-col1, col2, col3, col4 = st.columns([2, 1, 1, 3])
+col1, col2, col3, col4, col5 = st.columns([2, 1, 1, 1, 3])
 with col1:
     generate_button = st.button("**Generate**", use_container_width=True)
-with col4:
+with col5:
     st.link_button("**What is Kaprekar's Constant?**", "https://en.wikipedia.org/wiki/6174")
 
 
